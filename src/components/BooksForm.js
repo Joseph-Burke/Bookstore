1cf92a95b8f8dc20d388b0fc6c64f3f9c458dc06
Joseph Burke <joseph_burke@outlook.com>
2021-01-07 16:43:23 +0000

    Implement BooksForm as a stateful component

--- src/components/BooksForm.js
+++ src/components/BooksForm.js
@@ -1,32 +1,55 @@
-import React from 'react';
+import React, { Component } from "react";
 
 const CATEGORIES = [
-  { name: 'Action', id: 1 },
-  { name: 'Biography', id: 2 },
-  { name: 'History', id: 3 },
-  { name: 'Horror', id: 4 },
-  { name: 'Kids', id: 5 },
-  { name: 'Learning', id: 6 },
-  { name: 'Sci-Fi', id: 7 },
+  { name: "Action", id: 1 },
+  { name: "Biography", id: 2 },
+  { name: "History", id: 3 },
+  { name: "Horror", id: 4 },
+  { name: "Kids", id: 5 },
+  { name: "Learning", id: 6 },
+  { name: "Sci-Fi", id: 7 }
 ];
 
-const BooksForm = () => (
-  <form>
-    <label htmlFor="title">
-      Book Title:
-      <input name="title" placeholder="Tom Sawyer" />
-    </label>
+export default class BooksForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      category: '',
+    };
+  }
 
-    <select>
-      {CATEGORIES.map(category => {
-        const { name, id } = category;
-        return <option key={id}>{name}</option>;
-      })}
-      ;
-    </select>
+  handleInput(event) {
+    const { target } = event;
+    const { id } = target;
+    this.setState({
+      title: (id === 'title' ? target.value : this.state.title ),
+      category: (id === 'category' ? target.value : this.state.category )
+    });
+  }
 
-    <button type="submit">Submit</button>
-  </form>
-);
+  render() {
+    return (
+      <form onChange={this.handleInput.bind(this)}>
+        <label htmlFor="title">
+          Book Title:
+          <input
+            id="title"
+            name="title"
+            placeholder="Tom Sawyer"
+          />
+        </label>
 
-export default BooksForm;
+        <select id="category">
+          {CATEGORIES.map(category => {
+            const { name, id } = category;
+            return <option key={id}>{name}</option>;
+          })}
+          ;
+        </select>
+
+        <button type="button">Submit</button>
+      </form>
+    );
+  }
+}
