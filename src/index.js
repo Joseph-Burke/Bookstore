9316920c0e749292c1382a081d2bcb0e01265a47
Joseph Burke <joseph_burke@outlook.com>
2021-01-07 13:09:49 +0000

    Implement index.js with initialState & redux store

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./containers/App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import combinedReducer from "./reducers/index";
+
+const initialState = {
+  books: [
+    {
+      id: 1,
+      title: "Pride and Prejudice",
+      category: "Classics"
+    },
+    {
+      id: 2,
+      title: "Harry Potter and the Chamber of Secrets",
+      category: "Children"
+    },
+    {
+      id: 3,
+      title: "Das Kapital",
+      category: "Non-fiction"
+    }
+  ]
+};
+
+const store = createStore(combinedReducer, initialState);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
