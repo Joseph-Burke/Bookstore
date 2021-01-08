b1aa447e8b1d26dbde941165171612514b86dde2
Joseph Burke <joseph_burke@outlook.com>
2021-01-08 09:29:21 +0000

    Implement CategoryFilter component and add to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import BooksList from '../containers/BooksList';
 import BooksForm from '../containers/BooksForm';
+import CategoryFilter from './CategoryFilter';
 
 const App = () => (
   <div>
+    <CategoryFilter />
     <BooksList />
     <BooksForm />
   </div>
