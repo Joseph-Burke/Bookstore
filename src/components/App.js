844fed78f5536cd748f8c2873696aed57db76a3d
Joseph Burke <joseph_burke@outlook.com>
2021-01-08 17:39:35 +0000

    Render CategoryFilter within BooksList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import BooksList from '../containers/BooksList';
 import BooksForm from '../containers/BooksForm';
-import CategoryFilter from './CategoryFilter';
 
 const App = () => (
   <div>
-    <CategoryFilter />
     <BooksList />
     <BooksForm />
   </div>
