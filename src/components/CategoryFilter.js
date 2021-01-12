138ef52fa4acdd1ca27ad715574ac60e1a9e73a5
Joseph Burke <joseph_burke@outlook.com>
2021-01-12 02:05:15 +0000

    Fix additional ESLint offence

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,15 +1,15 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const OPTIONS = [
-  { name: "All", id: 1 },
-  { name: "Action", id: 2 },
-  { name: "Biography", id: 3 },
-  { name: "History", id: 4 },
-  { name: "Horror", id: 5 },
-  { name: "Kids", id: 6 },
-  { name: "Learning", id: 7 },
-  { name: "Sci-Fi", id: 8 }
+  { name: 'All', id: 1 },
+  { name: 'Action', id: 2 },
+  { name: 'Biography', id: 3 },
+  { name: 'History', id: 4 },
+  { name: 'Horror', id: 5 },
+  { name: 'Kids', id: 6 },
+  { name: 'Learning', id: 7 },
+  { name: 'Sci-Fi', id: 8 },
 ];
 
 const CategoryFilter = props => {
@@ -23,14 +23,14 @@ const CategoryFilter = props => {
         <option selected disabled hidden key={0}>
           CATEGORIES
         </option>,
-        ...optionElements
+        ...optionElements,
       ]}
     </select>
   );
 };
 
 CategoryFilter.propTypes = {
-  handleChange: PropTypes.func.isRequired
+  handleChange: PropTypes.func.isRequired,
 };
 
 export default CategoryFilter;
